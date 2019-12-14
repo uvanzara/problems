3dd912fd3d2bf28126a938bb284462d0d1b4f07c
uvanzara <vanzara@outlook.com>
2019-12-14 15:10:31 -0800

    bug fixes

--- adventofcode/day2/part1.py
+++ adventofcode/day2/part1.py
@@ -2,14 +2,11 @@ with open('input.txt', 'r') as fh:
     line = fh.readlines()
 
 in_arr = line[0].split(',')
-
+in_arr = [int(x) for x in in_arr]
 print('array len = {}'.format(len(in_arr)))
 
 idx = 0
-while idx <= len(in_arr):
-    # opcode = in_arr[idx]
-    decode(in_arr, idx)
-    # print("elem = {}".format(elem))
+
 
 def decode(arr, index):
     if arr[index] == 1:
@@ -22,7 +19,21 @@ def decode(arr, index):
         sums = arr[index+1] + arr[index+2]
         mem_loc = arr[index+3]
         arr[mem_loc] = sums
+        return 4
     if op == "mult":
         sums = arr[index+1] + arr[index+2]
         mem_loc = arr[index+3]
         arr[mem_loc] = sums
+        return 4
+    if op == "terminate":
+        return -1
+
+
+while idx <= len(in_arr):
+    # opcode = in_arr[idx]
+    incr = decode(in_arr, idx)
+    if incr == -1:
+        break
+    idx += incr
+    # print("elem = {}".format(elem))
+print('final array = {}'.format(in_arr))
